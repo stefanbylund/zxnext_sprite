31f3eff947044374deb1d35eac78737a10621a84
stefanbylund <stefan.bylund99@gmail.com>
2017-09-04 21:03:32 +0200

    Updated zxnext_registers.h

--- include/zxnext_registers.h
+++ include/zxnext_registers.h
@@ -10,7 +10,7 @@
  * writable or both readable/writable.
  *
  * Some registers are accessible only during the initialization process (IPL
- * and/or config mode).
+ * and modules).
  ******************************************************************************/
 
 #ifndef _ZXNEXT_REGISTERS_H
@@ -79,8 +79,8 @@
 
 /*
  * (W) Set page RAM (only in config mode, not IPL):
- * bits 7-5 = Reserved, must be 0
- * bits 4-0 = RAM page mapped in 0x0000-0x3FFF (32 pages of 16K = 512K). Reset to 0 after a PoR or hard reset.
+ * bits 7-6 = Reserved, must be 0
+ * bits 5-0 = RAM page mapped in 0x0000-0x3FFF (64 pages of 16K = 1024K). Reset to 0 after a PoR or hard reset.
  */
 #define PAGE_RAM_REGISTER 4
 
@@ -88,7 +88,7 @@
  * (R/W) Peripheral 1 settings:
  * bits 7-6 = joystick 1 mode (00 = Sinclair, 01 = Kempston, 10 = Cursor). Reset to 0 after a PoR or hard reset.
  * bits 5-4 = joystick 2 mode (00 = Sinclair, 01 = Kempston, 10 = Cursor). Reset to 0 after a PoR or hard reset.
- * bit 3 = Enable enhanced ULA (1 = enabled). Reset to 0 after a PoR or hard reset.
+ * bit 3 = Reserved, must be 0.
  * bit 2 = 50/60 Hz mode (0 = 50 Hz, 1 = 60 Hz). Reset to 0 after a PoR or hard reset.
  * bit 1 = Enable scanlines (1 = enabled). Reset to 0 after a PoR or hard reset.
  * bit 0 = Enable scandoubler (1 = enabled). Reset to 1 after a PoR or hard reset.
@@ -126,7 +126,7 @@
  * bit 3 = Enable Specdrum/Covox (1 = enabled). Reset to 0 after a PoR or hard reset.
  * bit 2 = Enable Timex modes (1 = enabled). Reset to 0 after a PoR or hard reset.
  * bit 1 = Enable TurboSound (1 = enabled). Reset to 0 after a PoR or hard reset.
- * bit 0 = NTSC/PAL for ZX-Uno board (0 = NTSC). Reset to 0 after a PoR or hard reset.
+ * bit 0 = Reserved, must be 0.
  */
 #define PERIPHERAL_3_REGISTER 8
 
@@ -142,22 +142,29 @@
 /*
  * (R/W) Layer 2 RAM page:
  * bits 7-6 = Reserved, must be 0
- * bits 5-0 = SRAM page
+ * bits 5-0 = SRAM page, points to page 8 after a reset which means page 8, 9 and 10.
  */
-#define LAYER2_RAM_PAGE_REGISTER 19
+#define LAYER2_RAM_PAGE_REGISTER 18
 
 /*
- * (R/W) Layer 2 transparency color:
- * bits 7-4 = Reserved, must be 0
- * bits 3-0 = ULA transparency color (IGRB). Reset to 1000 (black with bright) after a reset.
+ * (R/W) Layer 2 shadow RAM page:
+ * bits 7-6 = Reserved, must be 0
+ * bits 5-0 = SRAM page, points to page 11 after a reset which means page 11, 12 and 13.
  */
-#define LAYER2_TRANSPARENCY_COLOR_REGISTER 20
+#define LAYER2_SHADOW_RAM_PAGE_REGISTER 19
+
+/*
+ * (R/W) Global transparency colour:
+ * bits 7-0 = Transparency colour value. Reset to 0xE3 after a reset.
+ */
+#define GLOBAL_TRANSPARENCY_COLOR_REGISTER 20
 
 /*
  * (R/W) Sprite and layers system:
- * bits 7-5 = Reserved, must be 0
- * bits 4-2 = Set layer priorities; S=sprites, L=layer 2 screen, U=ULA graphics.
- * Reset default is 000, sprites over layer 2 screen over ULA graphics.
+ * bit 7 = LoRes mode, 128 x 96 pixels with 256 colours (1 = enabled).
+ * bits 6-5 = Reserved, must be 0
+ * bits 4-2 = Set layer priorities; S=sprites, L=layer 2 screen, U=ULA screen.
+ * Reset default is 000, sprites over layer 2 screen over ULA screen.
  *   000 = S L U
  *   001 = L S U
  *   010 = S U L
@@ -167,7 +174,7 @@
  * bit 1 = Sprites on border (1 = yes). Reset to 0 after a reset.
  * bit 0 = Sprites visible (1 = visible). Reset to 0 after a reset.
  */
-#define SPRITE_SYSTEM_REGISTER 21
+#define SPRITE_LAYERS_SYSTEM_REGISTER 21
 
 /*
  * (R/W) Layer 2 offset x:
@@ -182,33 +189,33 @@
 #define LAYER2_OFFSET_Y_REGISTER 23
 
 /*
- * (R) Raster video line (MSB):
+ * (R) Active video line (MSB):
  * bits 7-1 = Reserved, always 0
- * bit 0 = Raster line MSB. Reset to 0 after a reset.
+ * bit 0 = Active line MSB. Reset to 0 after a reset.
  */
-#define RASTER_VIDEO_LINE_MSB_REGISTER 30
+#define ACTIVE_VIDEO_LINE_MSB_REGISTER 30
 
 /*
- * (R) Raster video line (LSB):
- * bits 7-0 = Raster line LSB (0-255). Reset to 0 after a reset.
+ * (R) Active video line (LSB):
+ * bits 7-0 = Active line LSB (0-255). Reset to 0 after a reset.
  */
-#define RASTER_VIDEO_LINE_LSB_REGISTER 31
+#define ACTIVE_VIDEO_LINE_LSB_REGISTER 31
 
 /*
- * (R/W) Raster line interrupt control:
+ * (R/W) Line interrupt control:
  * bit 7 = (R) INT flag, 1 = During INT (even if the CPU has interrupt disabled)
  * bits 6-3 = Reserved, must be 0
  * bit 2 = If 1 disables original ULA interrupt. Reset to 0 after a reset.
- * bit 1 = If 1 enables raster line interrupt. Reset to 0 after a reset.
- * bit 0 = Raster line interrupt value MSB. Reset to 0 after a reset.
+ * bit 1 = If 1 enables line interrupt. Reset to 0 after a reset.
+ * bit 0 = Line interrupt line value MSB. Reset to 0 after a reset.
  */
-#define RASTER_LINE_INTERRUPT_CONTROL_REGISTER 34
+#define LINE_INTERRUPT_CONTROL_REGISTER 34
 
 /*
- * (R/W) Raster line interrupt value LSB:
- * bits 7-0 = Raster line interrupt value LSB (0-255). Reset to 0 after a reset.
+ * (R/W) Line interrupt value LSB:
+ * bits 7-0 = Line interrupt line value LSB (0-255). Reset to 0 after a reset.
  */
-#define RASTER_LINE_INTERRUPT_VALUE_LSB_REGISTER 35
+#define LINE_INTERRUPT_VALUE_LSB_REGISTER 35
 
 /*
  * (W) High address of keymap:
@@ -231,10 +238,71 @@
 #define KEYMAP_HIGH_DATA_REGISTER 42
 
 /*
- * (W) Low data to keymap (when writing this register, the address is automatically incremented):
+ * (W) Low data to keymap (when writing this register, the address is auto-incremented):
  * bits 7-0 = LSB data
  */
 #define KEYMAP_LOW_DATA_REGISTER 43
+
+/*
+ * (R/W) LoRes offset x:
+ * bits 7-0 = X offset (0-255). Reset to 0 after a reset.
+ * Being only 128 pixels, this allows the display to scroll in "half-pixels",
+ * at the same resolution and smoothness as layer 2.
+ */
+#define LORES_OFFSET_X_REGISTER 50
+
+/*
+ * (R/W) LoRes offset y:
+ * bits 7-0 = Y offset (0-191). Reset to 0 after a reset.
+ * Being only 96 pixels, this allows the display to scroll in "half-pixels",
+ * at the same resolution and smoothness as layer 2.
+ */
+#define LORES_OFFSET_Y_REGISTER 51
+
+/*
+ * (W) Palette index:
+ * bits 7-0 = Select the palette index (0-255) to change the default colour for.
+ * Indexes 0 to 127 are for ink colours and indexes 128 to 255 are for paper
+ * colours (except in full ink colour mode, where all indexes 0 to 255 are ink
+ * colours except index 128 which is the paper colour). Border colours are the
+ * same as the paper colours 0 to 7, indexes 128 to 135, even in full ink colour
+ * mode.
+ */
+#define PALETTE_INDEX_REGISTER 64
+
+/*
+ * (W) Palette value:
+ * bits 7-0 = Colour for the palette index selected by the register
+ * PALETTE_INDEX_REGISTER. Format is RRRGGGBB.
+ * After a write, the palette index is auto-incremented to the next index.
+ * The changed palette remains until a hard reset.
+ */
+#define PALETTE_VALUE_REGISTER 65
+
+/*
+ * (W) Palette format:
+ * bits 7-0 = Index of the last ink colour entry in the palette. Reset to 15 after a reset.
+ * This number can be 1, 3, 7, 15, 31, 63, 127 or 255. The value 255 enables the
+ * full ink colour mode where all the palette entries are ink colours except
+ * index 128 which is the paper colour.
+ */
+#define PALETTE_FORMAT_REGISTER 66
+
+/*
+ * (W) Palette control:
+ * bits 7-2 = Reserved, must be 0
+ * bit 1 = Select the second palette.
+ * bit 0 = Disable the standard Spectrum flash feature to enable the extra colours.
+ * Reset to 0 after a reset.
+ */
+#define PALETTE_CONTROL_REGISTER 67
+
+/*
+ * (W) Palette value lower bit:
+ * bits 7-1 = Reserved, must be 0
+ * bit 0 = Set the lower blue bit colour for the current palette value.
+ */
+#define PALETTE_VALUE_LSB_REGISTER 68
 
 /*
  * (W) Debug LEDs (DE-1, DE-2 and Multicore boards only)
